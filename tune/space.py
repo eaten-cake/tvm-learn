c93a66b96fc268d3f890cd7f90020f4a864d974b
yrx <421626388@qq.com>
2025-02-25 19:23:54 +0800

    update

--- tune/space.py
+++ tune/space.py
@@ -6,6 +6,8 @@ from tvm.script import relax as R
 from tvm.meta_schedule import TuneContext
 from tvm.meta_schedule.schedule_rule import AutoInline
 from tvm.meta_schedule.space_generator import PostOrderApply
+from tvm.meta_schedule.builder import Builder, BuilderInput, BuilderResult
+from tvm.meta_schedule.runner import Runner, RunnerInput, RunnerResult, RunnerFuture
 
 N, M, K = 128, 128, 64
 A = te.placeholder((N, K), name="A")
@@ -20,8 +22,8 @@ args = {
     "mod": mod,
     "target": "llvm -num-cores=1",
     "space_generator": "post-order-apply",
-    # "search_strategy": "evolutionary",
-    "search_strategy": "my",
+    "search_strategy": "evolutionary",
+    # "search_strategy": "my",
 }
 
 context = TuneContext(**args)
@@ -40,7 +42,30 @@ candidates = context.generate_measure_candidates()
 
 print("Number of candidates: ", len(candidates))
 
+builder = Builder.create("local", max_workers=10)
+runner = Runner.create("local", max_workers=10)
 
+build_inputs = []
+for candidate in candidates:
+    build_inputs.append(BuilderInput(candidate.sch.mod, context.target))
+
+build_results = builder.build(build_inputs)
+
+run_inputs = []
+for candidate, build_result in zip(candidates, build_results):
+    run_inputs.append(RunnerInput(build_result.artifact_path, context.target.kind.name, candidate.args_info))
+
+futures = runner.run(run_inputs)
+
+print(futures[0].result().run_secs)
+
+run_results = []
+for future in futures:
+    run_results.append(future)
+
+
+
+context.post_tuning()
 
 # for schedule in schedules:
 #     schedule.show()
